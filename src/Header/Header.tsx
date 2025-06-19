5a9e64fd625b4ec2fdd77303c41f47f2bdf84843
Pawan-Pandey1 <pawanpandey.3002@gmail.com>
2025-06-19 18:47:17 +0530

    first commit

---
+++ src/Header/Header.tsx
@@ -0,0 +1,31 @@
+import { Avatar } from "@mantine/core";
+import {IconBell, IconRun, IconSettings } from "@tabler/icons-react";
+
+const Header=()=>{
+    return <div className="w-full bg-mine-shaft-950 px-6 text-white h-20 flex justify-between items-center ">
+        <div className="flex gap-1 items-center text-royal-purple-400">
+            <IconRun className="h-8 w-8 " stroke={2.5}/>
+            <div className="text-3xl font-semibold">ReferVerse</div>
+        </div>
+        <div className="flex gap-5">
+            <a href="">Find Job</a>
+            <a href="">Find Talent</a>
+            <a href="">Upload Jobs</a>
+            <a href="">About Us</a>
+        </div>
+        <div  className="flex gap-3 items-center">
+                <div className="flex items-center gap-2">
+                    <div>Pawan</div>
+                    <Avatar src="avatar.png" alt="it's me" />
+                </div>
+            <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+                <IconSettings stroke={1.5}/>
+            </div>
+            <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+                <IconBell  stroke={1.5}/>
+            </div>
+
+        </div>
+    </div>
+}
+export default Header;
