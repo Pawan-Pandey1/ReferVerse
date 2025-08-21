0506e8bb3e5b7aa71158523b41bd7ce8adfc82b2
Pawan-Pandey1 <pawanpandey.3002@gmail.com>
2025-08-21 17:15:54 +0530

    Job referrals page updated

--- src/Pages/FindReferrals.tsx
+++ src/Pages/FindReferrals.tsx
@@ -1,11 +1,14 @@
 import { Divider } from "@mantine/core";
 import SearchBar from "../FindReferrals/SearchBar";
+import Jobs from "../FindReferrals/Jobs";
 
 const FindReferrals=()=>{
-        return(
+    return(
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
         <Divider size="xs" mx="md"/>
         <SearchBar/>
+        <Divider size="xs" mx="md"/>
+        <Jobs/>
     </div>
     )
 
